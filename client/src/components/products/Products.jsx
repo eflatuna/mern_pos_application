6eea2826378845e237f0de41201c2d697e1df636
esra <esrabaskaya84@gmail.com>
2025-05-11 23:15:54 +0200

    feat: updated Products component with new layout and styles

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,5 +1,21 @@
 const Products = () => {
-	return <div>Products</div>;
+	return (
+		<div className="products-wrapper grid grid-cols-card gap-4 ">
+			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+				<div className="product-img">
+					<img
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
+						alt=""
+						className="h-28 object-cover w-full border-b"
+					/>
+				</div>
+				<div className="product-info flex flex-col p-3">
+					<span className="font-bold">Apple</span>
+					<span>2€</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Products;
