0fbab6467c1a6fe9cba293d09435b0533f06a824
esra <esrabaskaya84@gmail.com>
2025-05-13 07:44:57 +0200

    Added Button, Card, and updated Table component in CartPage.jsx

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,4 +1,4 @@
-import { Table } from "antd";
+import { Button, Card, Table } from "antd";
 import Header from "../components/Header/Header";
 
 const CartPage = () => {
@@ -39,7 +39,35 @@ const CartPage = () => {
 		<>
 			<Header />
 			<div className="px-6 ">
-				<Table dataSource={dataSource} columns={columns} bordered />
+				<Table
+					dataSource={dataSource}
+					columns={columns}
+					bordered
+					pagination={false}
+				/>
+				<div className="cart-total flex justify-end mt-4">
+					<Card className="w-72">
+						<div className="flex justify-between">
+							<span>Subtotal</span>
+							<span>100€</span>
+						</div>
+						<div className="flex justify-between my-2">
+							<span>VAT 20%</span>
+							<span className="text-dark-red">20€</span>
+						</div>
+						<div className="flex justify-between">
+							<b>Total</b>
+							<b>120€</b>
+						</div>
+						<Button
+							className="mt-4 w-full !bg-light-blue"
+							type="primary"
+							size="large"
+						>
+							Create an order
+						</Button>
+					</Card>
+				</div>
 			</div>
 		</>
 	);
