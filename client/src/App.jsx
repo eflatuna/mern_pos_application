0c7c939daa28609ef4e33795262ea21ab270e901
esra <esrabaskaya84@gmail.com>
2025-05-13 07:21:53 +0200

    Added CartPage component and route to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/home" element={<HomePage />} />
+				<Route path="/cart" element={<CartPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
