9971897b7aabeca993de50ee0dba46e3644e3286
esra <esrabaskaya84@gmail.com>
2025-05-13 07:30:32 +0200

    Added Ant Design Table component to CartPage component

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { Table } from "antd";
+import Header from "../components/Header/Header";
 
 const CartPage = () => {
-	return <div>CartPage</div>;
+	const dataSource = [
+		{
+			key: "1",
+			name: "Mike",
+			age: 32,
+			address: "10 Downing Street",
+		},
+		{
+			key: "2",
+			name: "John",
+			age: 42,
+			address: "10 Downing Street",
+		},
+	];
+
+	const columns = [
+		{
+			title: "Name",
+			dataIndex: "name",
+			key: "name",
+		},
+		{
+			title: "Age",
+			dataIndex: "age",
+			key: "age",
+		},
+		{
+			title: "Address",
+			dataIndex: "address",
+			key: "address",
+		},
+	];
+
+	return (
+		<>
+			<Header />
+			<div className="px-6 ">
+				<Table dataSource={dataSource} columns={columns} bordered />
+			</div>
+		</>
+	);
 };
 
 export default CartPage;
