7bbabdde20ce557484bc9863b8d8f05e251aac50
esra <esrabaskaya84@gmail.com>
2025-05-14 23:24:47 +0200

    Removed unused imports and form fields in PrintInvoice component

--- client/src/components/invoices/PrintInvoice.jsx
+++ client/src/components/invoices/PrintInvoice.jsx
@@ -1,29 +1,8 @@
-import { Button, Form, Input, Modal, Select, Card } from "antd";
-import React from "react";
+import { Modal } from "antd";
 
 const PrintInvoice = ({ setIsModalOpen, isModalOpen }) => {
-	const onFinish = (values) => {
-		console.log("Success:", values);
-	};
-
 	const closeModal = () => setIsModalOpen(false);
 
-	const formFields = [
-		{
-			label: "Customer name",
-			name: "customerName",
-			placeholder: "Enter customer's full name",
-			message: "Enter customer's full name",
-			component: <Input />,
-		},
-		{
-			label: "Phone number",
-			name: "phoneNumber",
-			placeholder: "Enter customer's phone number",
-			message: "Enter customer's phone number",
-			component: <Input />,
-		},
-	];
 	return (
 		<Modal
 			title="Print Invoice"
