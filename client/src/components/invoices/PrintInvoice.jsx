7d0592b8dbf0ae754e65c23f9cf679a0b68dc681
esra <esrabaskaya84@gmail.com>
2025-05-16 21:58:57 +0200

    Refactor PrintInvoice component layout and styles

--- client/src/components/invoices/PrintInvoice.jsx
+++ client/src/components/invoices/PrintInvoice.jsx
@@ -9,22 +9,53 @@ const PrintInvoice = ({ setIsModalOpen, isModalOpen }) => {
 			open={isModalOpen}
 			footer={false}
 			onCancel={closeModal}
+			width={800}
 		>
 			<section className="py-20 bg-black">
-				<div className="max-w-5xl mx-auto bg-white px-6">
+				<div className="max-w-4xl bg-white px-6">
 					<article className="overflow-hidden">
-						<div className="logo">
-							<h2 className="text-4xl font-bold text text-slate-700">
+						<div className="logo my-6">
+							<h2 className="text-4xl font-bold text-slate-700">
 								LOGO
 							</h2>
 							<div className="invoice-details">
-								<div>
-									<div className="text-md">
-										<p className="font-bold">
-											Invoice Details:
+								<div className="grid grid-cols-4 gap-12">
+									<div className="text-md text-slate-500">
+										<p className="font-bold text-slate-700">
+											Invoice Details
 										</p>
-										Unwrapped <p>Fake Street 123</p>
+										<p> Unwrapped </p>
+										<p>Fake Street 123</p>
 										<p>San Javier</p>
+										<p>CA 12345</p>
+									</div>
+									<div className="text-md text-slate-500">
+										<p className="font-bold text-slate-700">
+											Invoice
+										</p>
+										<p> The Boring Company</p>
+										<p>Tesla Street 123</p>
+										<p>Frisco</p>
+										<p>CA 1200</p>
+									</div>
+									<div className="text-md text-slate-500">
+										<p className="font-bold text-slate-700">
+											Invoice Nummer
+										</p>
+										<p>00041</p>
+										<p className="font-bold text-slate-700">
+											Date of Issue
+										</p>
+										<p>2022-01-01</p>
+									</div>
+									<div className="text-md text-slate-500">
+										<p className="font-bold text-slate-700">
+											Terms:
+										</p>
+										<p>0 Day</p>
+										<p className="font-bold text-slate-700">
+											Due
+										</p>
 										<p>CA 12345</p>
 									</div>
 								</div>
