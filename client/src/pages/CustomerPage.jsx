f94a6b9e179e1e5bdd7f51db92d7e7672cf919c5
esra <esrabaskaya84@gmail.com>
2025-05-18 11:46:03 +0200

    Added CustomerPage component and corresponding route to App.jsx

---
+++ client/src/pages/CustomerPage.jsx
@@ -0,0 +1,75 @@
+import { Button, Card, Table } from "antd";
+import Header from "../components/Header/Header";
+import { useState } from "react";
+import PrintInvoice from "../components/invoices/PrintInvoice";
+
+const CustomerPage = () => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const dataSource = [
+		{
+			key: "1",
+			name: "Mike",
+			age: 32,
+			address: "10 Downing Street",
+		},
+		{
+			key: "2",
+			name: "John",
+			age: 42,
+			address: "10 Downing Street",
+		},
+	];
+
+	const columns = [
+		{
+			title: "Name",
+			dataIndex: "name",
+			key: "name",
+		},
+		{
+			title: "Age",
+			dataIndex: "age",
+			key: "age",
+		},
+		{
+			title: "Address",
+			dataIndex: "address",
+			key: "address",
+		},
+	];
+
+	return (
+		<>
+			<Header />
+			<div className="px-6 ">
+				<h1 text-4xl font-bold text-center mb-4>
+					Invoices
+				</h1>
+				<Table
+					dataSource={dataSource}
+					columns={columns}
+					bordered
+					pagination={false}
+				/>
+				<div className="cart-total flex justify-end mt-4">
+					<Card className="w-72">
+						<Button
+							className="mt-4 w-full !bg-light-blue"
+							type="primary"
+							size="large"
+							onClick={() => setIsModalOpen(true)}
+						>
+							Print
+						</Button>
+					</Card>
+				</div>
+				<PrintInvoice
+					isModalOpen={isModalOpen}
+					setIsModalOpen={setIsModalOpen}
+				/>
+			</div>
+		</>
+	);
+};
+
+export default CustomerPage;
