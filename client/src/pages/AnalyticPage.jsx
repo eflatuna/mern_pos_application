3fcf4263f72fef251e8122270375d81bedaa66e3
esra <esrabaskaya84@gmail.com>
2025-05-18 12:33:59 +0200

    Removed unused images and updated Analytics page layout

--- client/src/pages/AnalyticPage.jsx
+++ client/src/pages/AnalyticPage.jsx
@@ -9,20 +9,27 @@ const AnalyticPage = () => {
 					Analytics
 				</h1>
 				<div className="analytic-section">
-					<h2 className="text-xl">
-						Welcome
+					<h2 className="text-lg">
+						Welcome{" "}
 						<span className="text-light-green font-bold text-xl">
 							Admin
 						</span>
 					</h2>
 					<div className="analytic-cards grid grid-cols-4">
-						<div className="card-item bg-gray-800">
-							<div>
-								<div>
-									<img src="" alt="" />
+						<div className="card-item bg-gray-800 p-8 rounded-lg">
+							<div className="flex gap-x-2">
+								<div className="rounded-full bg-white w-16 h-16 p-3">
+									<img src="images/user.png" alt="" />
+								</div>
+								<div className="text-white">
+									<p className="mb-2 text-lg font-medium text-gray-400">
+										Total Customers
+									</p>
+									<p className="text-xl font-semibold text-gray-200">
+										125
+									</p>
 								</div>
 							</div>
-							<div></div>
 						</div>
 					</div>
 				</div>
