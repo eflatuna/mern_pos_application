f8cd68937392184e662fe5b07e0b97c147ccf5ed
esra <esrabaskaya84@gmail.com>
2025-05-18 12:23:37 +0200

    Added analytic section with welcome message and grid layout

--- client/src/pages/AnalyticPage.jsx
+++ client/src/pages/AnalyticPage.jsx
@@ -8,13 +8,23 @@ const AnalyticPage = () => {
 				<h1 className="text-4xl font-bold text-center mb-4">
 					Analytics
 				</h1>
-				<div>
+				<div className="analytic-section">
 					<h2 className="text-xl">
 						Welcome
 						<span className="text-light-green font-bold text-xl">
 							Admin
 						</span>
 					</h2>
+					<div className="analytic-cards grid grid-cols-4">
+						<div className="card-item bg-gray-800">
+							<div>
+								<div>
+									<img src="" alt="" />
+								</div>
+							</div>
+							<div></div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
