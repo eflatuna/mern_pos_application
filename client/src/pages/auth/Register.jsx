dab4c6b3d7dced1d2c919e88af16050d2b5bb6ef
esra <esrabaskaya84@gmail.com>
2025-05-21 23:16:27 +0200

    Added Ant Design Form to Register page and basic layout structure

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,5 +1,18 @@
+import { Form } from "antd";
 const Register = () => {
-	return <div>Register</div>;
+	return (
+		<div className="h-screen">
+			<div className="flex justify-between h-full">
+				<div className="left">
+					<div className="flex flex-col h-full justify-center">
+						<h1 className="text-center text-5xl font-bold">LOGO</h1>
+						<Form></Form>
+					</div>
+				</div>
+				<div className="right"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
