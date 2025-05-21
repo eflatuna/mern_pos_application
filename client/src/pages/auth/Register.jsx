a0a0d85727916544e3d7c9c45286a494701493cc
esra <esrabaskaya84@gmail.com>
2025-05-21 23:17:28 +0200

    Added logo and input field to the Register page

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,12 +1,18 @@
-import { Form } from "antd";
+import { Form, Input } from "antd";
 const Register = () => {
 	return (
 		<div className="h-screen">
 			<div className="flex justify-between h-full">
 				<div className="left">
 					<div className="flex flex-col h-full justify-center">
-						<h1 className="text-center text-5xl font-bold">LOGO</h1>
-						<Form></Form>
+						<h1 className="text-center text-5xl font-bold mb-2">
+							LOGO
+						</h1>
+						<Form>
+							<Form.Item>
+								<Input />
+							</Form.Item>
+						</Form>
 					</div>
 				</div>
 				<div className="right"></div>
