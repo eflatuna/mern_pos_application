72bdcfe0b0326cc0724a1c0bb6ae465956da0cdc
esra <esrabaskaya84@gmail.com>
2025-05-22 09:39:34 +0200

    Added form fields for username, email, password, and confirm password in Register.jsx

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -4,13 +4,59 @@ const Register = () => {
 		<div className="h-screen">
 			<div className="flex justify-between h-full">
 				<div className="left">
-					<div className="flex flex-col h-full justify-center">
+					<div className="px-10 xl:px-20 flex flex-col h-full justify-center">
 						<h1 className="text-center text-5xl font-bold mb-2">
 							LOGO
 						</h1>
-						<Form>
-							<Form.Item>
+						<Form layout="vertical">
+							<Form.Item
+								label="Username"
+								name={"username"}
+								rules={[
+									{
+										required: true,
+										message: "Please input your username!",
+									},
+								]}
+							>
 								<Input />
+							</Form.Item>
+							<Form.Item
+								label="Email"
+								name={"email"}
+								rules={[
+									{
+										required: true,
+										message: "Please input your Email!",
+									},
+								]}
+							>
+								<Input />
+							</Form.Item>
+							<Form.Item
+								label="Password"
+								name={"password"}
+								rules={[
+									{
+										required: true,
+										message: "Please input your Password!",
+									},
+								]}
+							>
+								<Input.Password />
+							</Form.Item>
+							<Form.Item
+								label="Confirm Password"
+								name={"confirmPassword"}
+								rules={[
+									{
+										required: true,
+										message:
+											"Please re-enter your password to confirm!",
+									},
+								]}
+							>
+								<Input.Password />
 							</Form.Item>
 						</Form>
 					</div>
