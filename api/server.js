d2fb4de3817f6f95118ed0da4303af1abff1ffe9
esra <esrabaskaya84@gmail.com>
2025-05-23 11:16:35 +0200

    Added MongoDB and Mongoose dependencies and configuration to the project.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,22 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}:)`));
+const connect = async () => {
+	try {
+		await mongoose.connect(
+			"mongodb+srv://esrabaskaya84:e.1234@cluster0.kllqwsq.mongodb.net/"
+		);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		throw error;
+	}
+};
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => {
+	connect();
+	console.log(`Server started on port ${port}`);
+});
