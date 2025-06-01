f7712c1739b99c78bdca96a45a496528069768ae
esra <esrabaskaya84@gmail.com>
2025-06-01 09:55:00 +0200

    Invoices.js ist created

---
+++ api/routes/invoices.js
@@ -0,0 +1,24 @@
+const invoice = require("../models/invoice.js");
+const express = require("express");
+const router = express.Router();
+
+router.get("/get-all", async (req, res) => {
+	try {
+		const invoices = await invoice.find();
+		res.status(200).json(invoices);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.post("/add-invoice", async (req, res) => {
+	try {
+		const newInvoice = new invoice(req.body);
+		await newInvoice.save();
+		res.status(200).json("invoice has been created");
+	} catch (error) {
+		res.status(400).json(error);
+	}
+});
+
+module.exports = router;
