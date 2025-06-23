560ea802e687af02f0fd7128fe0db7c2ea07fc99
esra <esrabaskaya84@gmail.com>
2025-06-23 11:20:06 +0200

    `Added password validation for login endpoint`

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -27,6 +27,18 @@ router.post("/login", async (req, res) => {
 	try {
 		const user = await User.findOne({ email: req.body.email });
 		!user && req.status(404).send({ error: "User not found" });
+
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+
+		if (!validPassword) {
+			res.status(403).send("Invalid Password!");
+		} else {
+			res.status(200).json(user);
+		}
+
 		res.send(user);
 	} catch (error) {
 		res.status(400).json(error);
