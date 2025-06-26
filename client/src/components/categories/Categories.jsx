b8737c5194eeb6d88512a020634d2fda4caaf27a
esra <esrabaskaya84@gmail.com>
2025-06-26 08:42:30 +0200

    `Refactored Categories component to use AddCategory component instead of Modal and Form`

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,27 +1,10 @@
 import { useState } from "react";
 import "./style.css";
 import { PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Input, message, Modal } from "antd";
+
+import AddCategory from "./AddCategory";
 const Categories = ({ categories, setCategories }) => {
 	const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-	const [form] = Form.useForm();
-
-	const onFinish = (values) => {
-		try {
-			fetch("http://localhost:5000/api/categories/add-category", {
-				method: "POST",
-				body: JSON.stringify(values),
-				headers: {
-					"Content-Type": "application/json; charset=UTF-8",
-				},
-			});
-			message.success("Category has been created");
-			form.resetFields();
-			setCategories([...categories, values]);
-		} catch (error) {
-			console.log(error);
-		}
-	};
 
 	return (
 		<ul className="flex gap-4 md:flex-col text-center text-lg ">
@@ -36,33 +19,12 @@ const Categories = ({ categories, setCategories }) => {
 					onClick={() => setIsAddModalOpen(true)}
 				/>
 			</li>
-			<Modal
-				title="Add New Category"
-				closable={{ "aria-label": "Custom Close Button" }}
-				open={isAddModalOpen}
-				onCancel={() => setIsAddModalOpen(false)}
-				footer={false}
-			>
-				<Form layout="vertical" onFinish={onFinish} form={form}>
-					<Form.Item
-						name={"title"}
-						label="Add Category"
-						rules={[
-							{
-								required: true,
-								message: "Category cannot be empty!",
-							},
-						]}
-					>
-						<Input placeholder="Add Category" />
-					</Form.Item>
-					<Form.Item className="flex justify-end mb-0">
-						<Button type="primary" htmlType="submit">
-							Create
-						</Button>
-					</Form.Item>
-				</Form>
-			</Modal>
+			<AddCategory
+				isAddModalOpen={isAddModalOpen}
+				setIsAddModalOpen={setIsAddModalOpen}
+				setCategories={setCategories}
+				categories={categories}
+			/>
 		</ul>
 	);
 };
