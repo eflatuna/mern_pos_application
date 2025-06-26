88eb673fed0e71ab21598ae0453e4f7fce7decb3
esra <esrabaskaya84@gmail.com>
2025-06-26 12:15:31 +0200

    `Added React hooks for fetching products and rendering them dynamically`

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,149 +1,40 @@
+import { useEffect, useState } from "react";
+
 const Products = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const getProducts = async () => {
+			try {
+				const res = await fetch(
+					"http://localhost:5000/api/products/get-all"
+				);
+				const data = await res.json();
+				setProducts(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getProducts();
+	}, []);
+
 	return (
 		<div className="products-wrapper grid grid-cols-card gap-4 ">
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
+			{products.map((item) => (
+				<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+					<div className="product-img">
+						<img
+							src={item.img}
+							alt=""
+							className="h-28 object-cover w-full border-b"
+						/>
+					</div>
+					<div className="product-info flex flex-col p-3">
+						<span className="font-bold">{item.title}</span>
+						<span>{item.price}</span>
+					</div>
 				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
-			<div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-				<div className="product-img">
-					<img
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyyRD7OkXlJDE9hgjrKwZ30rwLqwOaJbMiQ&s"
-						alt=""
-						className="h-28 object-cover w-full border-b"
-					/>
-				</div>
-				<div className="product-info flex flex-col p-3">
-					<span className="font-bold">Apple</span>
-					<span>2€</span>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 };
