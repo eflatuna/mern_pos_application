980fa5507d6fd2af8fc5e56dd7970586f416733b
esra <esrabaskaya84@gmail.com>
2025-06-30 23:07:40 +0200

    `Added Ant Design message component and updated ProductItem component layout`

--- client/src/components/products/ProductItem.jsx
+++ client/src/components/products/ProductItem.jsx
@@ -1,30 +1,36 @@
 import { addProduct } from "../../redux/cartSlice";
 import { useDispatch } from "react-redux";
+import { message } from "antd";
 
 const ProductItem = ({ item }) => {
 	const dispatch = useDispatch();
+	const [messageApi, contextHolder] = message.useMessage();
 
 	const handleClick = () => {
 		dispatch(addProduct({ ...item, quantity: 1 }));
+		messageApi.success("Product added to cart");
 	};
 
 	return (
-		<div
-			className="product-item border hover:shadow-lg cursor-pointer transition-all select-none"
-			onClick={handleClick}
-		>
-			<div className="product-img">
-				<img
-					src={item.img}
-					alt=""
-					className="h-28 object-cover w-full border-b"
-				/>
+		<>
+			{contextHolder}
+			<div
+				className="product-item border hover:shadow-lg cursor-pointer transition-all select-none"
+				onClick={handleClick}
+			>
+				<div className="product-img">
+					<img
+						src={item.img}
+						alt=""
+						className="h-28 object-cover w-full border-b"
+					/>
+				</div>
+				<div className="product-info flex flex-col p-3">
+					<span className="font-bold">{item.title}</span>
+					<span>{item.price}</span>
+				</div>
 			</div>
-			<div className="product-info flex flex-col p-3">
-				<span className="font-bold">{item.title}</span>
-				<span>{item.price}</span>
-			</div>
-		</div>
+		</>
 	);
 };
 
