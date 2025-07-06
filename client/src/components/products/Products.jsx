b438d73ad90c02408a9382db379519e06e8b9a48
esra <esrabaskaya84@gmail.com>
2025-07-06 18:50:04 +0200

    `Removed unused useEffect hook and import of useNavigate from the Products component`

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ProductItem from "./ProductItem";
 import { EditOutlined, PlusOutlined } from "@ant-design/icons";
 import AddProduct from "./AddProduct";
@@ -7,21 +7,6 @@ import { useNavigate } from "react-router-dom";
 const Products = ({ categories, filtered, products, setProducts }) => {
 	const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 	const navigate = useNavigate("/products");
-
-	useEffect(() => {
-		const getProducts = async () => {
-			try {
-				const res = await fetch(
-					"http://localhost:5000/api/products/get-all"
-				);
-				const data = await res.json();
-				setProducts(data);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		getProducts();
-	}, []);
 
 	return (
 		<div className="products-wrapper grid grid-cols-card gap-4 ">
