32c0eef59bad037f83ed8061f0ddf3d2b36b250a
esra <esrabaskaya84@gmail.com>
2025-07-06 14:23:29 +0200

    `Refactored App component to use Outlet and PrivateRoute from react-router-dom v6`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+	BrowserRouter,
+	Navigate,
+	Route,
+	Routes,
+	Outlet,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import InvoicePage from "./pages/InvoicePage";
@@ -8,36 +14,30 @@ import ProductPage from "./pages/ProductPage";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 
+// ProtectedRoute artık children yerine Outlet kullanıyor
+const PrivateRoute = () => {
+	const isAuth = Boolean(localStorage.getItem("posUser"));
+	return isAuth ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route
-					path="/"
-					element={
-						<RouteController>
-							<HomePage />
-						</RouteController>
-					}
-				/>
-				<Route path="/cart" element={<CartPage />} />
-				<Route path="/invoices" element={<InvoicePage />} />
-				<Route path="/customers" element={<CustomerPage />} />
-				<Route path="/analytics" element={<AnalyticPage />} />
-				<Route path="/products" element={<ProductPage />} />
-				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+
+				<Route element={<PrivateRoute />}>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/cart" element={<CartPage />} />
+					<Route path="/invoices" element={<InvoicePage />} />
+					<Route path="/customers" element={<CustomerPage />} />
+					<Route path="/analytics" element={<AnalyticPage />} />
+					<Route path="/products" element={<ProductPage />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
 }
 
 export default App;
-
-export const RouteController = ({ children }) => {
-	if (localStorage.getItem("posUser")) {
-		return children;
-	} else {
-		return <Navigate to="/login" />;
-	}
-};
