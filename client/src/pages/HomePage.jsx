25f82b924255053c849e9671ac00c475c8b4cf83
esra <esrabaskaya84@gmail.com>
2025-07-07 10:07:42 +0200

    `Added conditional rendering for loading state and updated component structure`

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,10 +3,11 @@ import Header from "../components/Header/Header";
 import CartTotal from "../components/cart_totals/CartTotal";
 import Categories from "../components/categories/Categories";
 import Products from "../components/products/Products";
+import { Spin } from "antd";
 
 const HomePage = () => {
-	const [categories, setCategories] = useState([]);
-	const [products, setProducts] = useState([]);
+	const [categories, setCategories] = useState();
+	const [products, setProducts] = useState();
 	const [filtered, setFiltered] = useState([]);
 	const [search, setSearch] = useState("");
 
@@ -48,28 +49,35 @@ const HomePage = () => {
 	return (
 		<>
 			<Header setSearch={setSearch} />
-			<div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24 h-screen">
-				<div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
-					<Categories
-						categories={categories}
-						setCategories={setCategories}
-						setFiltered={setFiltered}
-						products={products}
-					/>
+			{products && categories ? (
+				<div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24 h-screen">
+					<div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
+						<Categories
+							categories={categories}
+							setCategories={setCategories}
+							setFiltered={setFiltered}
+							products={products}
+						/>
+					</div>
+					<div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10 min-h-[500px]">
+						<Products
+							categories={categories}
+							filtered={filtered}
+							products={products}
+							setProducts={setProducts}
+							search={search}
+						/>
+					</div>
+					<div className="cart-wrapper min-w-[300px] md:-mr-[24px] border">
+						<CartTotal />
+					</div>
 				</div>
-				<div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10 min-h-[500px]">
-					<Products
-						categories={categories}
-						filtered={filtered}
-						products={products}
-						setProducts={setProducts}
-						search={search}
-					/>
-				</div>
-				<div className="cart-wrapper min-w-[300px] md:-mr-[24px] border">
-					<CartTotal />
-				</div>
-			</div>
+			) : (
+				<Spin
+					size="large"
+					className="absolute w-screen h-screen flex items-center justify-center z-50"
+				/>
+			)}
 		</>
 	);
 };
